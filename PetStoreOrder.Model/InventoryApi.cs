aa536416883960c319544f32d81ded2d2454d196
Kevin Lussier <kclussier@gmail.com>
2019-01-10 07:06:52 -0800

    Add files via upload

---
+++ PetStoreOrder.Model/InventoryApi.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetStoreOrder.Model
+{
+    public class InventoryApi
+    {
+        "statusCode": 200,
+        "body": [
+            {
+                "id": 100,
+                "name": "leash",
+                "price": 9.99
+            },
+            {
+                "id": 101,
+                "name": "collar",
+                "price": 6.99
+            },
+            {
+                "id": 102,
+                "name": "food bowl",
+                "price": 14.99
+            },
+            {
+                "id": 103,
+                "name": "squeaky toy",
+                "price": 4.99
+            },
+            {
+                "id": 104,
+                "name": "dog bed",
+                "price": 19.99
+            }
+        ]
+    }
+}
